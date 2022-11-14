7b711b6cd1454160192a6660c31402dc37f90230
Raquel Paradinha <raquelparadinha@gmail.com>
2022-11-14 15:16:41 +0000

    Don't tell me you're too blind to see

--- Projeto1/app/models.py
+++ Projeto1/app/models.py
@@ -7,7 +7,7 @@ class Patient(models.Model):
     address = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user.username + " " + self.email + " " + self.address
+        return self.user.username
 
 class Department(models.Model):
     name = models.CharField(max_length=50, unique=True)
